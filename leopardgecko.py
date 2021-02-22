3530398fa28133fa1b9eef5d679d87d91c950361
Luis Perdigao <luis.perdigao@rfi.ac.uk>
2021-02-22 21:59:10 +0000

    Update to leopardgecko.py and Report-making code. It now shows the score of pre-segmented volumes

--- leopardgecko.py
+++ leopardgecko.py
@@ -105,7 +105,16 @@ class PredictedData:
 
     @staticmethod
     def AvgPool3DPytorch(data3d_np , kwidth=8 , stride0=1):
+        '''
+        Applies Pytorch AvgPool3D on the numpy data object data3d_np with the width and stride parameters given
+        Automatically uses GPU if available.
+        This function will not check if the data is too large. Use this with caution
+        It is recommended that data3d_np has a maximum of 512x512x512 size, to keep GPU usage acceptably low
+        The function returns the average-pooled data as a numpy 3D array objects
+        '''
         #Generic. It will use the GPU if available
+        
+        #Avoid using this function
         if torch.cuda.is_available():
                 dev="cuda:0"
         else:
@@ -195,7 +204,6 @@ class PredictedData:
         torch.cuda.empty_cache()
         
         return datavol_avg, (iz_da_min , iz_da_max , iy_da_min , iy_da_max , ix_da_min , ix_da_max)
-
 
     def AvgPool3D_LargeData(self, w_avg = 512, k_width=256 , s_stride=8 , do_weighting = True):
         #This function will do the avarage pooling in 3D using PyTorch AvgPool3D
@@ -300,6 +308,14 @@ class PredictedData:
         return res
     
 
+    def getWeightedValueAverageOfVolume(self, coordsTuple):
+        zmin, zmax , ymin,ymax, xmin,xmax  = coordsTuple
+        #It does not use the score data from the average pooling
+        #But uses the actual combined data (predicted data)
+        vol_da = self.weightedData_da[zmin:zmax, ymin:ymax , xmin:xmax]
+        cscore = da.average(vol_da).compute()
+        return cscore
+        
     @staticmethod
     def SelfTest():
         #TODO: Write some code here that tests this class
@@ -403,7 +419,7 @@ class ScoreData:
         return self.histogram , self.histogram_bins
 
 
-    def SelectRegionsOfInterest_V0(self , showPlot=True , printReport = True):
+    def SelectRegionsOfInterest_V0(self , showPlot=True , printReport = True, k_width=256 ):
         if self.histogram is None:
             _a, _b = self.getHistogram()
         
@@ -411,7 +427,7 @@ class ScoreData:
         hist_x_vmax = self.histogram_bins [ np.where(self.histogram == hist_vmax)[0] ]
         
         hist_xmin = self.data3d_vmin
-        hist_xmax = self.data3d_vmin
+        hist_xmax = self.data3d_vmax
 
         #Choose regions based in the histogram shape
         #Approximate peak with gaussian
@@ -422,22 +438,20 @@ class ScoreData:
         mguess = hist_x_vmax[0]
         popt, pcov = optimize.curve_fit(fgaussian, self.histogram_bins , self.histogram, [aguess, mguess, 1.0])
 
-        #Commonest consistency
-        hx5 = popt[1]
+        print("Gaussian fit to peak, parameters amplitude={} , mean={} ,stdev={}".format(popt[0],popt[1],popt[2]))
 
-        #At FWHM going up
-        hx4 = popt[1] - popt[2]*2.35482 /2 # FHWM = 2.35482*stdev 
+        hx = []
 
-        #At mean - 3*stdev (3 sigma)
-        hx3 = popt[1] - 3* popt[2]
-
-        #Somewhere halfway between max and min
-        hx2 = (hist_xmax + hist_xmin)/2.0
-
-        hx1 = (hist_xmax - hist_xmin)/4 + hist_xmin
+        hx.append ( (hist_xmax - hist_xmin)/4 + hist_xmin )
+        hx.append ( (hist_xmax + hist_xmin)/2.0 )
+        hx.append ( popt[1] - 3* popt[2] )  #At mean - 3*stdev (3 sigma)
+        hx.append ( popt[1] - popt[2]*2.35482 /2 ) # FHWM = 2.35482*stdev, At FWHM going up
+        hx.append ( popt[1] ) #Commonest consistency
 
         def getClosestlVoxelFromListOfIndices(listindices_c, Zc,Yc,Xc):
             #From the list of voxel indices, get the closest voxel to point (Xc, Yc, Zc)
+            #Also returns the distance and its score
+
             listindices = listindices_c[0]
             listcscores = listindices_c[1]
             
@@ -464,65 +478,76 @@ class ScoreData:
         Ycenter = int( (np.amax( self.yVolCentres ) - np.amin( self.yVolCentres )) /2 )
         Xcenter = int( (np.amax( self.xVolCentres ) - np.amin( self.xVolCentres )) /2 )
 
-        #TODO: This can be simplified in a for loop
-        point5 = getClosestlVoxelFromListOfIndices ( \
-            self.GetIndicesFromOrigDataWithScoreNear( hx5, popt[2] / 4 ), \
-            Zcenter , Ycenter, Xcenter)
+        pointsOfInterest=[]
 
-        point4 = getClosestlVoxelFromListOfIndices ( \
-            self.GetIndicesFromOrigDataWithScoreNear( hx4, popt[2] / 4 ), \
-            Zcenter , Ycenter, Xcenter)
+        for hx0 in hx:
+            pointsOfInterest.append( \
+                getClosestlVoxelFromListOfIndices ( \
+                self.GetIndicesFromOrigDataWithScoreNear( hx0, popt[2] / 4 ), \
+                Zcenter , Ycenter, Xcenter) )
+
+        # #TODO: This can be simplified in a for loop
+        # point5 = 
+
+        # point4 = getClosestlVoxelFromListOfIndices ( \
+        #     self.GetIndicesFromOrigDataWithScoreNear( hx4, popt[2] / 4 ), \
+        #     Zcenter , Ycenter, Xcenter)
         
-        point3 = getClosestlVoxelFromListOfIndices ( \
-            self.GetIndicesFromOrigDataWithScoreNear( hx3, popt[2] / 4 ), \
-            Zcenter , Ycenter, Xcenter)
+        # point3 = getClosestlVoxelFromListOfIndices ( \
+        #     self.GetIndicesFromOrigDataWithScoreNear( hx3, popt[2] / 4 ), \
+        #     Zcenter , Ycenter, Xcenter)
 
-        point2 = getClosestlVoxelFromListOfIndices ( \
-            self.GetIndicesFromOrigDataWithScoreNear( hx2, popt[2] / 4 ), \
-            Zcenter , Ycenter, Xcenter)
+        # point2 = getClosestlVoxelFromListOfIndices ( \
+        #     self.GetIndicesFromOrigDataWithScoreNear( hx2, popt[2] / 4 ), \
+        #     Zcenter , Ycenter, Xcenter)
 
-        point1 = getClosestlVoxelFromListOfIndices ( \
-            self.GetIndicesFromOrigDataWithScoreNear( hx1, popt[2] / 4 ), \
-            Zcenter , Ycenter, Xcenter)
+        # point1 = getClosestlVoxelFromListOfIndices ( \
+        #     self.GetIndicesFromOrigDataWithScoreNear( hx1, popt[2] / 4 ), \
+        #     Zcenter , Ycenter, Xcenter)
 
-        pointsOfInterest = (point1, point2, point3, point4, point5)
-
-        #Consider removal
-        if printReport:
-            def point_ReportResultAs_Vol_Center_CScore(p3d , width):
-                w2 = int(width/2)
-                
-                p = p3d[0] #First element is the voxel coordinates
-                point_dist = p3d[1] #Second element is the distance to...
-                point_cscore = p3d[2] #Second element is the consistency score
-                
-                s = "[ " + str(p[0]-w2) + ":" + str(p[0]+w2) + " , " + \
-                    str(p[1]-w2) + ":" + str(p[1]+w2) + " , " + \
-                    str(p[2]-w2) + ":" + str(p[2]+w2) + \
-                    " ]"
-                s +=  " centered at " + str(p)
-                s += " cscore=" + str(point_cscore)
-                return s
-
-            print("Gaussian fit to peak, parameters amplitude={} , mean={} ,stdev={}".format(popt[0],popt[1],popt[2]))
-
-            print ("Volume region with bad consistency " , point_ReportResultAs_Vol_Center_CScore(point1, 256) )
-            print ("Volume region with bad-mid consistency " , point_ReportResultAs_Vol_Center_CScore(point2, 256) ) 
-            print ("Volume region with mid consistency " , point_ReportResultAs_Vol_Center_CScore(point3, 256) )
-            print ("Volume region with half-commonest consistency " , point_ReportResultAs_Vol_Center_CScore(point4, 256) )
-            print ("Volume region with commonest consistency " , point_ReportResultAs_Vol_Center_CScore(point5, 256) )
+        # pointsOfInterest = (point1, point2, point3, point4, point5)
 
         #Consider removal
-        if showPlot:
-            plt.plot(self.histogram_bins, self.histogram)
-            plt.xlabel('Score')
-            plt.ylabel('Counts')
-            plt.axvline(point1[2], label='point1', color='red')
-            plt.axvline(point2[2], label='point2', color='red')
-            plt.axvline(point3[2], label='point3', color='red')
-            plt.axvline(point4[2], label='point4', color='red')
-            plt.axvline(point5[2], label='point5', color='red')
+        # if printReport:
+        #     def point_ReportResultAs_Vol_Center_CScore(p3d , width):
+        #         w2 = int(width/2)
+                
+        #         p = p3d[0] #First element is the voxel coordinates
+        #         point_dist = p3d[1] #Second element is the distance to...
+        #         point_cscore = p3d[2] #Second element is the consistency score
+                
+        #         s = "[ " + str(p[0]-w2) + ":" + str(p[0]+w2) + " , " + \
+        #             str(p[1]-w2) + ":" + str(p[1]+w2) + " , " + \
+        #             str(p[2]-w2) + ":" + str(p[2]+w2) + \
+        #             " ]"
+        #         s +=  " centered at " + str(p)
+        #         s += " cscore=" + str(point_cscore)
+        #         return s
 
+        #     for i in range( len(pointsOfInterest) ):
+        #         print ("Point {}".format(i) , " , ", point_ReportResultAs_Vol_Center_CScore( pointsOfInterest[i] , k_width) )
+
+        #Consider removal
+        # if showPlot:
+        #     plt.plot(self.histogram_bins, self.histogram)
+        #     plt.xlabel('Score')
+        #     plt.ylabel('Counts')
+
+        #     for i in range( len(pointsOfInterest) ):
+        #         x0=pointsOfInterest[i][2] #gets the point score
+        #         plt.axvline( x0, color='red')
+        #         # plt.axvline(point2[2], label='point2', color='red')
+        #         # plt.axvline(point3[2], label='point3', color='red')
+        #         # plt.axvline(point4[2], label='point4', color='red')
+        #         # plt.axvline(point5[2], label='point5', color='red')
+
+        #         #Annotate lines
+        #         plt.annotate( 'point{}'.format(i), ( x0, fgaussian(x0, popt[0], popt[1], popt[2]) ) )
+        #         #uses the gaussian approximation to get a rough location of the point
+        #         #TODO: this is not ingenious enough and prone to errors. Try to use raw score data for this
+
+        #         if extraX is not None:
+        #             plt.axvline( extraX, color='green')
 
         return pointsOfInterest
 
