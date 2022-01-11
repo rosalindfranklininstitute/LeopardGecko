37130b02ea1f7df17267c84b0e2bfecaf20f3e24
Luis Perdigao <perdigao1@yahoo.com>
2022-01-11 16:16:45 +0000

    rafactor metrics

--- leopardgecko/Pcrit.py
+++ leopardgecko/Pcrit.py
@@ -216,63 +216,6 @@ class MultiClassMultiWayPredictOptimizer:
     METRICACCURACY=0
     METRICDICE=1
 
-    def MetricScoreOfVols_Accuracy(self, vol0, vol1):
-        '''
-        Get the whole-volume Accuracy metric between two volumes that have been segmented
-        '''
-        equalvol = np.equal(vol0,vol1).astype(np.float32)
-        
-        res = equalvol.mean()
-        return res
-
-    def MetricScoreOfVols_Dice(self, vol0, vol1):
-        '''
-        Get the whole-volume Multiclass metric between two volumes that have been segmented
-        '''
-        
-        logging.info("MetricScoreOfVols_Dice()")
-        #Check both volumes dtype is int
-
-        if not ( np.issubdtype(vol0.dtype, np.integer) and np.issubdtype(vol1.dtype, np.integer) ):
-            logging.info("Volumes are not integer type. Try to convert them")
-            vol0 = vol0.astype(np.int8)
-            vol1= vol1.astype(np.int8)
-
-            #equalvol = np.equal(vol0,vol1).astype(np.float32)
-            #res = equalvol.mean()
-
-        #Number of segmentations
-        nseg0 = np.max(vol0)
-        nseg1 = np.max(vol1)
-        logging.info(f"Number of class segmentations in first volume {nseg0+1}")
-        logging.info(f"Number of class segmentations in second volume {nseg1+1}")
-
-        if nseg0 != nseg1:
-            logging.warning ("Number of segmentations between volumes is different.")
-        
-        nseg = max(nseg0, nseg1)
-
-        isegstart=1
-        if self.useBckgnd: isegstart=0
-        
-        #Calculate dice of each metric
-        #Similar to code in https://github.com/fastai/fastai/blob/master/fastai/metrics.py#L343
-        dicescores=np.array([])
-        for iseg in range(isegstart,nseg+1): #include last value , discards background (iseg=0)
-            p = np.where(vol0 == iseg, 1, 0)
-            t = np.where(vol1 == iseg, 1, 0)
-            c_inter = (p*t).astype(np.float32).sum()
-            c_union = (p+t).astype(np.float32).sum()
-
-            #dicescore= 2.*self.inter[c]/self.union[c] if self.union[c] > 0 else np.nan
-            dicescore= 2.*c_inter/c_union if c_union > 0 else np.nan
-
-            dicescores = np.append(dicescores, dicescore)
-
-        dicescore_all = np.nanmean(dicescores)
-
-        return dicescore_all
-
 
     def getMetricScoreWithPcritFromClassVols(self, a_all, gt_rnd , metric=METRICDICE):
         '''
@@ -289,12 +232,11 @@ class MultiClassMultiWayPredictOptimizer:
 
         if metric == self.METRICDICE:
             logging.info(f"metric= Dice , useBckgnd = {self.useBckgnd}")
-            score=self.MetricScoreOfVols_Dice(classed_vol,gt_rnd)
+            score = MetricScoreOfVols_Dice(classed_vol,gt_rnd)
         elif metric == self.METRICACCURACY:
             logging.info("metric= Accuracy")
-            score = self.MetricScoreOfVols_Accuracy(classed_vol, gt_rnd)
+            score = MetricScoreOfVols_Accuracy(classed_vol, gt_rnd)
         
-        #TODO: Consider adding other metrics
 
         return score , classed_vol
 
--- leopardgecko/metrics.py
+++ leopardgecko/metrics.py
@@ -1,81 +1,61 @@
-
 import numpy as np
 #import matplotlib.pyplot as plt #Needed here?
 import dask.array as da
 import logging
 
-def MetricAccuracyWholeVolume ( data1_da, data2_da ):
+def MetricScoreOfVols_Accuracy(vol0, vol1):
     '''
-    Calculates Accuracy metric of the whole volume by using the formula:
-
-    Accuracy = (data1==data2).sum / data1(2).volume
-
-    where (data1==data2) is volume where voxels have values for 1 when d1(z,y,x) = d2(z,,y,x) and zero otherwise
-
-    Parameters:
-    data1_da , data2_da : Volume data to compare, often with integer values per voxel representing class/segmentation.
-    Data type is daskarray format
-
-    Returns:
-    A single value corresponding to the Accuracy score between the two data sets across the whole volume
-
+    Get the whole-volume Accuracy metric between two volumes that have been segmented
     '''
-    #This code will not check whether the data is boolean or not.
+    equalvol = np.equal(vol0,vol1).astype(np.float32)
+    
+    res = equalvol.mean()
+    return res
 
-    logging.info("MetricAccuracyWholeVolume")
-
-    #check shapes of data1 and data2 are the same
-    if (data1_da.shape == data2_da.shape ):
-        #Both data has the same shape
-
-        logging.info ("Calculating elementwise d1==d2")
-        neq_d1d2_bool= da.equal(data1_da , data2_da)
-
-        #Convert
-        neq_d1d2 = neq_d1d2_bool.astype('float')
-
-        #Calculates the Accuracy-metric coefficient as the mean of the whole array
-        ret = da.mean(neq_d1d2).compute()
-        
-        return ret
-    else:
-        return None
-
-def MetricSorensenDiceCoefficientWholeVolume (data1bool_da, data2bool_da ):
+def MetricScoreOfVols_Dice(vol0, vol1, useBckgnd=False):
     '''
-    Calculates Sorensen-Dice coefficient by using the formula:
-
-    SDC = 2* (data1bin_da * data2bin_da).sum / (data1bin_da.sum() + data2bin_da.sum)
-
-    Parameters:
-    data1bool_da , data2bool_da : Volume data with zeros (background) and ones (mask) per voxel. Data type is daskarray
-
-    Returns:
-    A single value corresponding to the Sorensen-Dice score between the two data sets across the whole volume
-
+    Get the whole-volume Multiclass metric between two volumes that have been segmented
     '''
+    
+    logging.info("MetricScoreOfVols_Dice()")
+    #Check both volumes dtype is int
 
-    logging.info("MetricSorensenDiceCoefficientWholeVolume")
+    if not ( np.issubdtype(vol0.dtype, np.integer) and np.issubdtype(vol1.dtype, np.integer) ):
+        logging.info("Volumes are not integer type. Try to convert them")
+        vol0 = vol0.astype(np.int8)
+        vol1= vol1.astype(np.int8)
 
-    #This will not check whether the data is boolean or not.
-    #logging.info("SorensenDiceCoefficientCalcWholeVolume")
+        #equalvol = np.equal(vol0,vol1).astype(np.float32)
+        #res = equalvol.mean()
 
-    #Assumes data has values 0 or 1
-    #It will not work with multiple segmented images
+    #Number of segmentations
+    nseg0 = np.max(vol0)
+    nseg1 = np.max(vol1)
+    logging.info(f"Number of class segmentations in first volume {nseg0+1}")
+    logging.info(f"Number of class segmentations in second volume {nseg1+1}")
 
-    #check shapes of data1 and data2 are the same
-    if (data1bool_da.shape == data2bool_da.shape ):
-        #Both data has the same shape
+    if nseg0 != nseg1:
+        logging.warning ("Number of segmentations between volumes is different.")
+    
+    nseg = max(nseg0, nseg1)
 
-        im_intersection = (data1bool_da * data2bool_da).sum().compute()
-        im_sum = (data1bool_da.sum() + data2bool_da.sum()).compute()
+    isegstart=1
+    if useBckgnd: isegstart=0
+    
+    #Calculate dice of each metric
+    #Similar to code in https://github.com/fastai/fastai/blob/master/fastai/metrics.py#L343
+    dicescores=np.array([])
+    for iseg in range(isegstart,nseg+1): #include last value , discards background (iseg=0)
+        p = np.where(vol0 == iseg, 1, 0)
+        t = np.where(vol1 == iseg, 1, 0)
+        c_inter = (p*t).astype(np.float32).sum()
+        c_union = (p+t).astype(np.float32).sum()
 
-        if im_sum==0:
-            return None
+        #dicescore= 2.*self.inter[c]/self.union[c] if self.union[c] > 0 else np.nan
+        dicescore= 2.*c_inter/c_union if c_union > 0 else np.nan
 
-        #Calculates the Sorensen-Dice coefficient as the mean of the whole array
-        sdc_wholevol =  2.0 * im_intersection / im_sum
-        
-        return sdc_wholevol
-    else:
-        return None
+        dicescores = np.append(dicescores, dicescore)
+
+    dicescore_all = np.nanmean(dicescores)
+
+    return dicescore_all
