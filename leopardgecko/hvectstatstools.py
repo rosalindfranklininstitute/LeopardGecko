68e85417953e663da22d36c0acd11b8415c66c80
Luis <perdigao1@yahoo.com>
2023-04-27 08:07:53 +0000

    Fix and try new counting algorithms & New N2C func

--- leopardgecko/hvectstatstools.py
+++ leopardgecko/hvectstatstools.py
@@ -85,50 +85,53 @@ def hvect_gndclass_counter(data_all , data_class_gnd , hvectors):
 
 #     return hvect_gndclass_counter0
 
-def gethvect_gndclass_counter_in_data_counted(data, data_gnd, hvectors):
-    '''
-    Does some counting statistics of hvector,gndclass.
-    This is useful to analyse the quality of predictions from Unet, and may help with assignment of hvector-to-classlabel
 
-    Parameters:
-        data: volume where each voxeld has the counted number of volumes that gave non-background
-        data_class_gnd: data volume with the ground truth class assignement for respective voxels
-        hvectors: a list of tuples with all the possible hvectors
-    Returns:
-        hvect_gndclass_counter: dictionary with each entry being hvector: [c0,c1,c2]
-            with c0 c1 and c2 being the counts for each class
+# TODO: REQUIRES REVISION. IT APPEARS IT WORKS ONLY WITH 2-CLASS DATA
+# def gethvect_gndclass_counter_in_data_counted(countdata_np, data_gnd, hvectors):
+#     '''
+#     Does some counting statistics of hvector,gndclass.
+#     This is useful to analyse the quality of predictions from Unet, and may help with assignment of hvector-to-classlabel
+
+#     Parameters:
+#         data: volume where each voxeld has the counted number of volumes that gave non-background
+#         data_class_gnd: data volume with the ground truth class assignement for respective voxels
+#         hvectors: a list of tuples with all the possible hvectors
+#     Returns:
+#         hvect_gndclass_counter: dictionary with each entry being hvector: [c0,c1,c2]
+#             with c0 c1 and c2 being the counts for each class
     
-    This version is much faster than the previous version
-    See: scripts/developing_hvectstatstools.ipynb
-    '''
+#     This version is much faster than the previous version
+#     See: scripts/developing_hvectstatstools.ipynb
+#     '''
 
-    nclasses = len(hvectors[0])
-    nways = int(np.sum(np.array(hvectors[0],dtype=np.uint8)))
+#     nclasses = len(hvectors[0])
+#     nways = int(np.sum(np.array(hvectors[0],dtype=np.uint8)))
 
-    #Initialise the counter
-    hvect_gndclass_counter0 = {}
-    for h0 in hvectors:
-        d0= []
-        for c0 in range(nclasses):
-            d0.append(0)
-        hvect_gndclass_counter0[ h0 ] = d0
+#     #Initialise the counter
+#     hvect_gndclass_counter0 = {}
+#     for h0 in hvectors:
+#         d0= []
+#         for c0 in range(nclasses):
+#             d0.append(0)
+#         hvect_gndclass_counter0[ h0 ] = d0
 
-    data_gnd_binary = (data_gnd!=0)
+#     data_gnd_binary = (data_gnd!=0)
 
-    #iterate calculation per vector basis
-    for i0 in range(nways+1):
-        hv0 = (i0, nways-i0)
+#     #iterate calculation per vector basis
+#     for i0 in range(nways+1):
+#         hv0 = (i0, nways-i0)#I am not sure what is happening here, this seems to be only applicable to 2-class data
 
-        #get matches
-        d_match = (data==nways-i0)
-        match_count = np.count_nonzero(d_match)
+#         #get matches
+#         d_match = (data==nways-i0)
+#         match_count = np.count_nonzero(d_match)
 
-        #Compare with gntruth
-        count_compare_to_gnd = np.count_nonzero(np.logical_and(d_match,data_gnd_binary))
+#         #Compare with gntruth
+#         count_compare_to_gnd = np.count_nonzero(np.logical_and(d_match,data_gnd_binary))
 
-        hvect_gndclass_counter0[hv0] = [ match_count- count_compare_to_gnd, count_compare_to_gnd]
+#         hvect_gndclass_counter0[hv0] = [ match_count- count_compare_to_gnd, count_compare_to_gnd]
 
-    return hvect_gndclass_counter0
+
+#     return hvect_gndclass_counter0
 
 def hvect_count_in_data(data_all, hvectors):
     '''
@@ -150,6 +153,7 @@ def hvect_count_in_data(data_all, hvectors):
 
     zsize = data_all.shape[1]
 
+    #TODO: WARNING THIS ONLY SUPPORTS 3-CLASS
     for iz in range(zsize):
         logging.info(f"iz = {iz} / {zsize}")
         zslice = data_all[:,iz,:,:]
@@ -163,28 +167,28 @@ def hvect_count_in_data(data_all, hvectors):
     return hvect_counter2, hvect_counter2_max
 
 
-def hvect_count_in_data_counted(countdata_np, hvectors):
-    '''
-    Does some counting statistics of values inside the data_np
-    Parameters:
-        countdata_np: volume with the counted occurences of non-background prediction per voxel
-        hvectors: a list of tuples with all the possible hvectors
-    Returns:
-        hvect_count_in_data0 : dictionary with each entry being {hvector:count}
-        hvect_count_in_data0_max : maximum count value. It can be useful for setting up plots
-    '''
-    #Initialise the counter
-    hvect_count_in_data0={} #dictionary hvector to counts
-    hvect_count_in_data0_max=0
+# def hvect_count_in_data_counted(countdata_np, hvectors):
+#     '''
+#     Does some counting statistics of values inside the data_np
+#     Parameters:
+#         countdata_np: volume with the counted occurences of non-background prediction per voxel
+#         hvectors: a list of tuples with all the possible hvectors
+#     Returns:
+#         hvect_count_in_data0 : dictionary with each entry being {hvector:count}
+#         hvect_count_in_data0_max : maximum count value. It can be useful for setting up plots
+#     '''
+#     #Initialise the counter
+#     hvect_count_in_data0={} #dictionary hvector to counts
+#     hvect_count_in_data0_max=0
 
-    for h0 in hvectors:
-        v0 = h0[1]
-        count = np.count_nonzero(countdata_np == v0)
-        print(f"hvector: {h0} , count:{count}")
-        hvect_count_in_data0[h0]=count
-        hvect_count_in_data0_max = max(hvect_count_in_data0_max, count )
+#     for h0 in hvectors:
+#         v0 = h0[1]
+#         count = np.count_nonzero(countdata_np == v0)
+#         print(f"hvector: {h0} , count:{count}")
+#         hvect_count_in_data0[h0]=count
+#         hvect_count_in_data0_max = max(hvect_count_in_data0_max, count )
 
-    return hvect_count_in_data0, hvect_count_in_data0_max
+#     return hvect_count_in_data0, hvect_count_in_data0_max
 
 
 def get_hvect_combinations(nclasses, nways):
@@ -277,3 +281,87 @@ def get_fast_metrics(hvect_gnd_class_counter, hvect_to_class):
         "accuracy_score": accuracy_value,
         "dice_scores": dicescores
         }
+
+
+def hvect_gndclass_counter1(data_all , data_class_gnd , hvectors):
+    '''
+    Prototyping
+
+    Does some counting statistics of hvector,gndclass.
+    This is useful to analyse the quality of predictions from Unet, and may help with assignment of hvector-to-classlabel
+
+    Parameters:
+        data_all: Several data volumes, with the first dimension being the index of each of the data volumes
+        data_class_gnd: data volume with the ground truth class assignement for respective voxels
+        hvectors: a list of tuples with all the possible hvectors
+    Returns:
+        hvect_gndclass_counter: dictionary of hvector:tuple with the tuple being the number of occurences of
+            the indexed class of that h-vector in the ground truth volume
+    '''
+    nclasses = len(hvectors[0])
+
+    #Initialise the counter
+    #Another way to handle data
+    hvect_gndclass_counter1 = {}
+    for h0 in hvectors:
+        d0= []
+        for c0 in range(nclasses):
+            d0.append(0)
+        hvect_gndclass_counter1[ h0 ] = d0
+
+    #For each h-vector, get locations where the h-vector appears
+    for h0 in hvectors:
+        print("hvector:", h0)
+        d_h0_locs=None
+        for i, h0_el in enumerate(h0):
+            if i==0: # initialise for the first class
+                d_h0_locs = (data_all[i,:,:,:]==h0_el)
+            else:
+                d_h0_locs = ( ( data_all[i,:,:,:]==h0_el) & d_h0_locs )
+
+        #By this point d_h0_locs should be boolean volume where hvector appears        
+        #Compare with ground truth
+
+        # match= np.where(d_h0_locs, 1,np.nan)
+
+        # masked_gnd = match*data_class_gnd #Mask in such a way that outside the mak will be NaN
+
+        # for cl0 in range(nclasses):
+        #     count_in_gnd_for_cls = np.sum(np.where(masked_gnd==cl0,1,0))
+        #     hvect_gndclass_counter1[ h0 ][cl0] += count_in_gnd_for_cls
+
+        #Try to use masked array
+        masked_gnd = np.ma.masked_where(np.logical_not(d_h0_locs), data_class_gnd)
+        for cl0 in range(nclasses):
+            count_in_gnd_for_cls = np.ma.sum(np.ma.where(masked_gnd==cl0,1,0))
+            if count_in_gnd_for_cls is np.ma.masked:
+                count_in_gnd_for_cls=0
+            hvect_gndclass_counter1[ h0 ][cl0] += count_in_gnd_for_cls
+
+    return hvect_gndclass_counter1
+
+def get_n2c_from_hvect_count_in_data_max(hvect_gndclass_count_in_data0_l):
+    '''
+    From a hvect_gndclass_count_in_data data type ( a dictionary with tuples to a list of counts in ground truth), gets the
+    class that gives the maximum count for each hvector.
+
+    Example: if dictionary has entry (3,4,5):[35,64,20] , class index 1 has maximum counts, then the corresponding result in the dictionary returned will be
+    (3,4,5):1
+
+    In some cases, the counts are all zero, and there is not enough information to work out the maximum.
+    (11,1,0):[30,0,0] will then return (11,1,0):-1
+
+    '''
+
+    hvect_to_class_dict = {}
+    for hvect, d0 in hvect_gndclass_count_in_data0_l.items():
+        #Get max wedge
+        d0_np = np.array(d0)
+        max = np.max(d0_np)
+        if max>0:  #Check if there is any data that is enough for classification based on highest counts
+            imax = np.argmax(d0_np) #This will be the class that will be selected
+            hvect_to_class_dict[hvect] = imax
+        else:
+            hvect_to_class_dict[hvect] = -1
+
+    return hvect_to_class_dict
